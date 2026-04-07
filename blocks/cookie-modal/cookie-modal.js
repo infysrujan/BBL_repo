9a74df52b3c26915ddf21946538b01d422c5fc2e
tejasvi-j <tejasvij@adobe.com>
2026-04-07 16:57:45 +0530

    initial commit

---
+++ blocks/cookie-modal/cookie-modal.js
@@ -0,0 +1,8 @@
+/**
+ * Cookie Modal Block
+ * @param {Element} block - The cookie modal block element
+ */
+export default function decorate(block) {
+  const rows = [...block.children];
+  console.log(rows);
+}
