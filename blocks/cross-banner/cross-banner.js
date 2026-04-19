51f3ab826ea9abd451e4d8ed1f16bff1ed2d8d18
Satarupa Das <satdas@adobe.com>
2026-04-20 04:36:01 +0530

    Js for cross banner smart crop

--- blocks/cross-banner/cross-banner.js
+++ blocks/cross-banner/cross-banner.js
@@ -1,6 +1,60 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+function createElement(tag, ...classNames) {
+  const el = document.createElement(tag);
+  if (classNames.length) el.classList.add(...classNames);
+  return el;
+}
+
+function createSmartImage(imageCellDesktop, imageCellMobile, imageAlt) {
+  const imgDesktop = imageCellDesktop?.querySelector('img');
+  const imgMobile = imageCellMobile?.querySelector('img');
+  // Extract alt text from a cell that may contain HTML
+  let altText = '';
+  if (imageAlt) {
+    // Try to get direct text (including from <p>, <span>, etc)
+    altText = imageAlt.textContent?.trim() || '';
+  }
+  if (imgDesktop && imgMobile) {
+    const picture = createElement('picture');
+
+    const desktopSource = createElement('source');
+    desktopSource.setAttribute('media', '(min-width: 761px)');
+    const desktopSrc = imgDesktop.getAttribute('src');
+    desktopSource.setAttribute('srcset', desktopSrc);
+
+    const mobileSource = createElement('source');
+    mobileSource.setAttribute('media', '(max-width: 760px)');
+    const mobileSrc = imgMobile.getAttribute('src');
+    mobileSource.setAttribute('srcset', mobileSrc);
+
+    const img = createElement('img');
+    img.setAttribute('loading', 'lazy');
+
+    img.setAttribute('alt', altText || img.alt || '');
+    img.setAttribute('src', mobileSrc);
+
+    picture.appendChild(desktopSource);
+    picture.appendChild(mobileSource);
+    picture.appendChild(img);
+
+    return picture;
+  }
+  const img = imgMobile || imgDesktop;
+  if (!img) return null;
+  return createOptimizedPicture(
+    img.src,
+    altText || '',
+    false,
+    [{ media: '(max-width: 760px)', width: '2000' }, { width: '750' }],
+  );
+}
+
 export default function decorate(block) {
-  const [imgEl, budgeTextEl, titleEl, descriptionEl, buttonEl] = block.children;
-  const pictureHTML = imgEl?.querySelector('img')?.outerHTML || '';
+  const [imgElDesktop, imgElMobile, imgAlt, budgeTextEl, titleEl, descriptionEl, buttonEl] = block.children;
+  // const pictureHTML = imgEl?.querySelector('img')?.outerHTML || '';
+  const picture = createSmartImage(imgElDesktop, imgElMobile, imgAlt);
+  const pictureHTML = picture?.outerHTML || '';
   const budgeText = budgeTextEl?.textContent?.trim() || '';
   const titleName = titleEl?.textContent?.trim() || '';
   const description = descriptionEl?.querySelector('p')?.innerHTML?.trim() || '';
