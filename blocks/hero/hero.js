787485c41abc958a11afbc8fdfeffede4544af2e
Sai Kunal BMD <sbysani@adobe.com>
2026-05-13 18:27:26 +0530

    add pretitle for hero block

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -64,6 +64,7 @@ export default function decorate(block) {
       imageAlt,
       logoImageCell,
       thumbImgCell,
+      preTitleCell,
       headingCell,
       textCell,
       linkCell,
@@ -96,8 +97,9 @@ export default function decorate(block) {
     }
 
     const contentGroup = createElement('div', 'hero-banner-content-group');
+    if (preTitleCell?.firstElementChild) preTitleCell.firstElementChild.classList.add('hero-banner-pre-title');
     if (textCell?.firstElementChild) textCell.firstElementChild.classList.add('hero-banner-content-inner-text');
-    [headingCell, textCell, linkCell].forEach((cell) => {
+    [preTitleCell, headingCell, textCell, linkCell].forEach((cell) => {
       if (cell) contentGroup.innerHTML += cell.innerHTML;
     });
     decorateButtonsV1(contentGroup);
