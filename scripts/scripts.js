a2eddb5e343700e5aca0e4788aee6d29ddfaba17
mathai <mathai@adobe.com>
2026-05-14 14:31:54 +0800

    Updated the delayed call to pass the object

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -278,7 +278,7 @@ async function loadLazy(doc) {
  */
 function loadDelayed() {
   // Load the gtm-martech library in the delayed phase.
-  window.setTimeout(gtmMartech.delayed, 1000);
+  window.setTimeout(() => void gtmMartech.delayed(), 1000);
   // eslint-disable-next-line import/no-cycle
   window.setTimeout(() => {
     // Load the martech library in the delayed phase.
