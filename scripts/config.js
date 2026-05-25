8a43bf82dd26f41ba6e4f26d3c8dc274ca632724
Sai Kunal BMD <sbysani@adobe.com>
2026-05-25 11:16:05 +0530

    test commit to see if config will sync

--- scripts/config.js
+++ scripts/config.js
@@ -25,6 +25,8 @@ export async function fetchConfigs() {
       const cachedConfigJSON = window.sessionStorage.getItem(configKey);
 
       if (cachedConfigJSON) {
+        // test comment to sync the config.js file
+
         try {
           const json = JSON.parse(cachedConfigJSON);
           const configs = {};
