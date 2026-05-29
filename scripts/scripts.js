530e20f695bbf0a2d6225ed19b11d2da5ed63499
mathai <mathai@adobe.com>
2026-05-29 13:10:51 +0800

    Updated launch script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -67,7 +67,7 @@ const dataStreamConfig = {
 // TODO: Update BBL's Launch script URLs here
 const launchConfig = {
   dev: [
-    'https://assets.adobedtm.com/0e4712067e10/8a225ec96e9d/launch-f3f44b9a5684-development.min.js',
+    'https://assets.adobedtm.com/0e4712067e10/931565ba35cd/launch-f69e7329c58a-development.min.js',
   ],
   stage: [
     'https://assets.adobedtm.com/0e4712067e10/8a225ec96e9d/launch-f3f44b9a5684-development.min.js',
