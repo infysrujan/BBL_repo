57fe5151ecba63862efe9e3f93c3806f8dbd16ac
Satarupa Das <satdas@adobe.com>
2026-06-11 11:18:18 +0530

    removed the load lazy for images

--- scripts/bbl-decorators.js
+++ scripts/bbl-decorators.js
@@ -473,7 +473,7 @@ function createPictureWithoutOptimization(
       picture.appendChild(source);
     } else {
       const img = document.createElement('img');
-      img.setAttribute('loading', eager ? 'eager' : 'lazy');
+      if (eager) img.setAttribute('loading', 'eager');
       img.setAttribute('alt', alt);
       picture.appendChild(img);
       img.setAttribute('src', `${pathname}`);
