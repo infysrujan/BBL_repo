7147674f4becfe2b2d6bc2469b6cb6fb3c8b6940
NITHYA-B-A28 <nithyaba@adobe.com>
2026-06-19 10:36:12 +0530

    footer-updates

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,6 +1,5 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
-import { getLang } from '../../scripts/scripts.js';
 
 // Breakpoint matching design system tokens (760px tablet, 1024px desktop)
 const DESKTOP_BREAKPOINT = 1024;
@@ -146,8 +145,7 @@ export default async function decorate(block) {
   // load footer as fragment
   let footerPath = '';
   if (document.querySelector('body.error-page')) {
-    const lang = getLang();
-    footerPath = `/${lang}/footer`;
+    footerPath = '/footer';
   } else {
     const footerMeta = getMetadata('footer');
     footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
