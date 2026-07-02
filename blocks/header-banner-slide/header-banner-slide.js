28c09f088cc8bf51a21da6746bde02b11eea6bc7
SuchitraYaraguudri12 <syaragudri@adobe.com>
2026-07-02 11:01:16 +0530

    reverted chages

--- blocks/header-banner-slide/header-banner-slide.js
+++ blocks/header-banner-slide/header-banner-slide.js
@@ -1,5 +1,4 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
-import { isAuthoringInstance } from '../../scripts/bbl-decorators.js';
 
 function initCarousel(carousel, track) {
   const realTotal = track.children.length;
@@ -129,8 +128,6 @@ function initCarousel(carousel, track) {
 }
 
 export default function decorate(block) {
-  if (isAuthoringInstance(block)) return;
-
   const [titleRow, autoScrollRow, scrollDelayRow, infiniteLoopRow, ...slideItems] = block.children;
 
   const title = titleRow?.children[0]?.textContent?.trim() || '';
