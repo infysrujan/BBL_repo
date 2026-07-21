189a9fbeb7f558746f1720204c66baf43816645b
Athul Ganesh <athulj@adobe.com>
2026-07-21 11:15:20 +0530

    js pulled from dev

--- blocks/forex-rates/forex-rates.js
+++ blocks/forex-rates/forex-rates.js
@@ -189,12 +189,7 @@ function printForexRates(block) {
     .forex-rates-table thead th:nth-child(2) { width: 40%; }
     .forex-rates-table tbody tr { height: auto; }
     .forex-rates-table tbody td {
-<<<<<<< HEAD
       padding-block: 0.15rem; padding-inline: 20px; font-size: 0.5rem; height: auto; vertical-align: middle;
-=======
-      padding: 0.15rem 0.4rem; font-size: 0.5rem; height: auto; vertical-align: middle;
-      font-family: var(--bbl-font-family-primary);
->>>>>>> 62e55874d64829d77ab892eb51ff4698f4589468
       border-bottom: 0.0625rem solid var(--bbl-color-black); border-right: 0.0625rem solid var(--bbl-color-black);
     }
     .forex-rates-table tbody td:last-child { border-right: none; }
