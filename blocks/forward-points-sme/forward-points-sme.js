91fa10956213f59d9e9d1f5f2453b11e23fe8242
Athul Ganesh <athulj@adobe.com>
2026-08-26 13:38:52 +0530

    by default last option selected

--- blocks/forward-points-sme/forward-points-sme.js
+++ blocks/forward-points-sme/forward-points-sme.js
@@ -498,10 +498,7 @@ function setupSection(
         getEndpointUrl('updateInDay', parsed.day, parsed.month, parsed.year),
       );
       state.updates = updates;
-      const stillExists = updates.some((item) => trimValue(item.Update) === state.selectedUpdate);
-      if (!stillExists) {
-        state.selectedUpdate = trimValue(updates[updates.length - 1]?.Update);
-      }
+      state.selectedUpdate = trimValue(updates[updates.length - 1]?.Update);
     } catch (e) {
       // silent
     }
